c7aaddd969205778afaf1b90815270f0c909365b
Divyanshu Dev <154589654+devdivyanshu1@users.noreply.github.com>
2025-05-03 00:20:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
 //your JS code here. If required.
+document.getElementById('form').addEventListener('submit', function(e) {
+  e.preventDefault(); 
+
+  const name = document.getElementById('name').value.trim();
+  const age = document.getElementById('age').value.trim();
+
+  if (name === "" || age === "") {
+    alert("Please enter valid details.");
+    return;
+  }
+
+  const userPromise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (parseInt(age) > 18) {
+        resolve(name);
+      } else {
+        reject(name);
+      }
+    }, 2000);
+  });
+
+  userPromise
+    .then(userName => {
+      alert(`Welcome, ${userName}. You can vote.`);
+    })
+    .catch(userName => {
+      alert(`Oh sorry ${userName}. You aren't old enough.`);
+    });
+});
